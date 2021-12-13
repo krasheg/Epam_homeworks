e98190f0a5d8c5b2d0bfef009c357304dbbac5f5
INlOEtdF <krasheg@gmail.com>
2021-12-13 22:06:38 +0200

    Added file operations tasks

---
+++ File_Operations/task10_ex_1.py
@@ -0,0 +1,20 @@
+"""
+Implement a function `sort_names(input_file_path: str, output_file_path: str) -> None`, which sorts names from
+`file_path` and write them to a new file `output_file_path`. Each name should start with a new line as in
+the following example:
+
+Example:
+Adele
+Adrienne
+...
+Willodean
+Xavier
+"""
+
+
+def sort_names(input_file_path: str, output_file_path: str) -> None:
+    with open(input_file_path, 'r') as input_file, open(output_file_path, 'w') as output_file:
+        all_names = input_file.read().strip()
+        names_list = all_names.split("\n")
+        sorted_names = "\n".join(sorted(names_list)) + '\n'
+        output_file.write(sorted_names)
---
+++ File_Operations/task10_ex_2.py
@@ -0,0 +1,23 @@
+"""Implement a function `most_common_words(file_path: str, top_words: int) -> list`
+which returns most common words in the file.
+<file_path> - system path to the text file
+<top_words> - number of most common words to be returned
+
+Example:
+
+print(most_common_words(file_path, 3))
+>> ['donec', 'etiam', 'aliquam']
+> NOTE: Remember about dots, commas, capital letters etc.
+"""
+import re
+from collections import Counter
+
+
+def most_common_words(file_path: str, top_words: int) -> list:
+    with open(file_path, 'r') as file:
+        text = file.read()
+    text = text.lower()
+    all_worlds = re.findall(r"\w+", text)
+    counter = Counter(all_worlds)
+    most_common = [value for value, count in counter.most_common(top_words)]
+    return most_common
